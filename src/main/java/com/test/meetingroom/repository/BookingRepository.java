a6f0ef1aad28c77a562193d8b3bb820ba19871c2
Otvos-Adam-Balazs <79146953+Otvos-Adam-Balazs@users.noreply.github.com>
2026-03-18 22:43:26 +0100

    add javadoc to repositories

--- src/main/java/com/test/meetingroom/repository/BookingRepository.java
+++ src/main/java/com/test/meetingroom/repository/BookingRepository.java
@@ -8,19 +8,38 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.test.meetingroom.entity.Booking;
 
-
+/**
+ * Repository for Booking entities. Contains custom queries for retrieving and checking bookings.
+ */
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
+  /**
+   * Finds all bookings for a specific meeting room.
+   *
+   * @param roomId the ID of the meeting room
+   * @return a list of {@link Booking} objects for the specified room
+   */
   @Query("SELECT b FROM Booking b WHERE b.room.id = :roomId")
   public List<Booking> findBookingsByMeetingRoomId(@Param("roomId") Integer roomId);
 
+  /**
+   * Retrieves all bookings in the system.
+   *
+   * @return a list of all {@link Booking} objects
+   */
   @Query("SELECT b FROM Booking b")
   public List<Booking> findBookings();
 
-  @Query("SELECT b FROM Booking b WHERE b.room.id = :roomId")
-  public List<Booking> deleteBooking(@Param("bookingId") Integer bookingId);
-
+  /**
+   * Checks whether there exists a booking for the given room that overlaps with a given time
+   * interval.
+   *
+   * @param roomId the ID of the meeting room
+   * @param startTime the start time of the new booking
+   * @param endTime the end time of the new booking
+   * @return true if there is a conflicting booking, false otherwise
+   */
   @Query("""
       SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END
       FROM Booking b
--- src/main/java/com/test/meetingroom/repository/MeetingRoomRepository.java
+++ src/main/java/com/test/meetingroom/repository/MeetingRoomRepository.java
@@ -7,12 +7,26 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.test.meetingroom.entity.MeetingRoom;
 
+/**
+ * Repository for MeetingRoom entities. Contains custom queries for retrieving meeting room data.
+ */
 @Repository
 public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Integer> {
 
+  /**
+   * Retrieves all meeting rooms from the database.
+   *
+   * @return a list of all {@link MeetingRoom} objects
+   */
   @Query("SELECT m FROM MeetingRoom m")
   public List<MeetingRoom> findMeetingRooms();
 
+  /**
+   * Retrieves a meeting room by its unique ID.
+   *
+   * @param meetingRoomId the ID of the meeting room
+   * @return the {@link MeetingRoom} with the given ID, or null if not found
+   */
   @Query("SELECT m FROM MeetingRoom m WHERE m.id = :meetingRoomId")
   public MeetingRoom findMeetingRoomById(@Param("meetingRoomId") Integer meetingRoomId);
 

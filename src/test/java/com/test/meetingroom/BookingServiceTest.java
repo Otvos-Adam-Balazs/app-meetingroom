637a94f75cd0817ea5768b29c54c2b27f17c0d5c
Otvos-Adam-Balazs <79146953+Otvos-Adam-Balazs@users.noreply.github.com>
2026-03-18 21:05:20 +0100

    refactor test

--- src/test/java/com/test/meetingroom/BookingServiceTest.java
+++ src/test/java/com/test/meetingroom/BookingServiceTest.java
@@ -7,11 +7,13 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.test.meetingroom.entity.Booking;
-import com.test.meetingroom.entity.MeetingRoom;
 import com.test.meetingroom.exception.BookingConflictException;
 import com.test.meetingroom.exception.BookingDataException;
 import com.test.meetingroom.exception.MeetingRoomNotFoundException;
@@ -20,6 +22,7 @@ import com.test.meetingroom.repository.MeetingRoomRepository;
 import com.test.meetingroom.service.BookingService;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BookingServiceTest {
 
   @Autowired
@@ -28,79 +31,101 @@ class BookingServiceTest {
   @Autowired
   private MeetingRoomRepository meetingRoomRepository;
 
+  @Test
+  @Order(1)
+  void testCreateValidBooking() {
+    assertEquals(0, bookingService.getBookings().size());
+
+    assertDoesNotThrow(() -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
+            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 0), ZoneOffset.UTC))
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 0), ZoneOffset.UTC))));
+
+    assertEquals(1, bookingService.getBookings().size());
+  }
 
   @Test
-  void testBookingFlow() {
-    List<MeetingRoom> meetingRooms = meetingRoomRepository.findMeetingRooms();
-    assertEquals(0, bookingService.getBookings().size());
-    bookingService.createBooking(new BookingRequest().roomId(1)
-        .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 0), ZoneOffset.UTC))
-        .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 0), ZoneOffset.UTC)));
-
-
-    assertThrows(BookingDataException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(1)
+  @Order(2)
+  void testInvalidBookingTimes() {
+    assertThrows(BookingDataException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
             .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 9, 0), ZoneOffset.UTC))
             .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 9, 0), ZoneOffset.UTC))));
 
-    assertThrows(BookingDataException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(1)
+    assertThrows(BookingDataException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
             .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 5, 1, 9, 0), ZoneOffset.UTC))
             .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 9, 0), ZoneOffset.UTC))));
+  }
 
-    assertThrows(BookingConflictException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(1)
+  @Test
+  @Order(3)
+  void testBookingConflictSameRoom() {
+
+    assertThrows(BookingConflictException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
+            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 30), ZoneOffset.UTC))
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 30), ZoneOffset.UTC))));
+
+    assertThrows(BookingConflictException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
             .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 9, 0), ZoneOffset.UTC))
-            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 30), ZoneOffset.UTC))));
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 12, 0), ZoneOffset.UTC))));
 
-    assertDoesNotThrow(
-        () -> bookingService.createBooking(new BookingRequest().roomId(2)
-            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 9, 0), ZoneOffset.UTC))
-            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 30), ZoneOffset.UTC))));
-
-    assertThrows(BookingConflictException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(1)
-            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 45), ZoneOffset.UTC))
-            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 45), ZoneOffset.UTC))));
-
-    assertDoesNotThrow(
-        () -> bookingService.createBooking(new BookingRequest().roomId(3)
-            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 45), ZoneOffset.UTC))
-            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 45), ZoneOffset.UTC))));
-
-    assertThrows(BookingConflictException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(1)
+    assertThrows(BookingConflictException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
             .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 15), ZoneOffset.UTC))
             .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 45), ZoneOffset.UTC))));
 
-    assertDoesNotThrow(
-        () -> bookingService.createBooking(new BookingRequest().roomId(4)
+    assertThrows(BookingConflictException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(1)
+            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 45), ZoneOffset.UTC))
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 45), ZoneOffset.UTC))));
+  }
+
+  @Test
+  @Order(4)
+  void testBookingDifferentRoomsNoConflict() {
+    assertDoesNotThrow(() -> bookingService.createBooking(
+        new BookingRequest().roomId(2)
+            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 9, 0), ZoneOffset.UTC))
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 30), ZoneOffset.UTC))));
+
+    assertDoesNotThrow(() -> bookingService.createBooking(
+        new BookingRequest().roomId(3)
+            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 45), ZoneOffset.UTC))
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 11, 45), ZoneOffset.UTC))));
+
+    assertDoesNotThrow(() -> bookingService.createBooking(
+        new BookingRequest().roomId(4)
             .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 15), ZoneOffset.UTC))
             .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 10, 45), ZoneOffset.UTC))));
 
+    assertDoesNotThrow(() -> bookingService.createBooking(
+        new BookingRequest().roomId(4)
+            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 13, 0), ZoneOffset.UTC))
+            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 14, 0), ZoneOffset.UTC))));
+  }
 
-    assertThrows(BookingConflictException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(1)
+  @Test
+  @Order(5)
+  void testBookingNonExistentRoom() {
+    assertThrows(MeetingRoomNotFoundException.class, () -> bookingService.createBooking(
+        new BookingRequest().roomId(5)
             .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 8, 0), ZoneOffset.UTC))
             .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 12, 0), ZoneOffset.UTC))));
+  }
 
-    assertThrows(MeetingRoomNotFoundException.class,
-        () -> bookingService.createBooking(new BookingRequest().roomId(5)
-            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 8, 0), ZoneOffset.UTC))
-            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 4, 1, 12, 0), ZoneOffset.UTC))));
+  @Test
+  @Order(6)
+  void testDeleteBooking() {
 
-    assertDoesNotThrow(
-        () -> bookingService.createBooking(new BookingRequest().roomId(4)
-            .startTime(OffsetDateTime.of(LocalDateTime.of(2026, 5, 1, 8, 0), ZoneOffset.UTC))
-            .endTime(OffsetDateTime.of(LocalDateTime.of(2026, 5, 1, 12, 0), ZoneOffset.UTC))));
+    List<Booking> bookings = bookingService.getBookingsByMeetingRoomId(4);
+    assertEquals(2, bookings.size());
 
-    assertEquals(2, bookingService.getBookingsByMeetingRoomId(4).size());
-
-    Booking booking = bookingService.getBookingsByMeetingRoomId(4).get(0);
-
-    bookingService.deleteBooking(booking.getId());
-
+    bookingService.deleteBooking((bookings.get(0)).getId());
     assertEquals(1, bookingService.getBookingsByMeetingRoomId(4).size());
-    assertEquals(4, bookingService.getBookings().size());
+
+    assertEquals(bookingService.getBookings().size(), 4);
   }
 }
